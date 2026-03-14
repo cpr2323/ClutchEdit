a35a1e9729e4c4df683b8829b7c8c1f2c5045412
Chris Roberts <cprandroid@gmail.com>
2026-03-14 16:27:41 -0700

    Added new border look to the pattern list page

--- Source/GUI/Clutch/PatternListEditorComponent.cpp
+++ Source/GUI/Clutch/PatternListEditorComponent.cpp
@@ -39,17 +39,20 @@ void PatternListEditorComponent::init (juce::ValueTree rootPropertiesVT)
 
 void PatternListEditorComponent::paintOverChildren (juce::Graphics& g)
 {
-    g.setColour (juce::Colours::black);
-    // TODO juce docs recommend using drawRect for horizontal and vertical lines
-    g.drawLine (juce::Line { patternLabels [0].getX (), patternEditors [0].getY () - 5,
-                             patternEditors [0].getRight (), patternEditors [0].getY () - 5 }.toFloat (), 1.0f);
+    const auto kSectionOutlineColour { juce::Colour (0xff6a6a6a) };
+    g.setColour (kSectionOutlineColour.brighter (0.4f));
+    constexpr auto kSectionCornerSize { 4.0f };
+    constexpr auto kSectionOutlineThickness { 1.0f };
     for (auto patternIndex { 0 }; patternIndex < patternEditors.size (); ++patternIndex)
     {
-        g.drawRect (patternLabels [patternIndex].getX (), patternEditors[patternIndex].getY () - 5,
-                    patternEditors[patternIndex].getRight () - patternLabels[patternIndex].getX (), patternEditors[patternIndex].getHeight());
+        auto& patternLabel { patternLabels [patternIndex] };
+        auto& patternEditor { patternEditors [patternIndex] };
+        const auto x { patternLabel.getX () + 5 };
+        const auto y { patternEditor.getY () };
+        const auto width { patternLabel.getWidth () + patternEditor.getWidth () - 70 };
+        const auto height { patternEditor.getHeight () - 5 };
+        g.drawRoundedRectangle (juce::Rectangle<int> (x, y, width, height).toFloat(), kSectionCornerSize, kSectionOutlineThickness);
     }
-    g.drawLine (juce::Line { patternLabels [7].getX (), patternEditors [7].getBottom () - 5,
-                             patternEditors [7].getRight (), patternEditors [7].getBottom () - 5 }.toFloat (), 1.0f);
 }
 
 void PatternListEditorComponent::resized ()
